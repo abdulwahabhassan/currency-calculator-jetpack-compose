1c012129c0a5af194d27b5b0e86707c9aafe0fd3
devhassan <abdulwahabhassan645@gmail.com>
2022-02-15 17:29:14 +0100

    clean up unused methods

--- data/src/main/java/com/hassan/data/api/FixerApi.kt
+++ data/src/main/java/com/hassan/data/api/FixerApi.kt
@@ -9,24 +9,6 @@ import retrofit2.http.Query
 
 interface FixerApi {
 
-    @GET("latest")
-    suspend fun convertRate(
-        @Query("base") base: String,
-        @Query("symbols") symbols: String
-    ) : Response<RatesResponse>
-
-    @GET("latest")
-    suspend fun getLatestRates(
-        @Query("base") base: String
-    ) : Response<RatesResponse>
-
-    @GET("{date}")
-    suspend fun getHistoricalRates(
-        @Path("date") date: String,
-        @Query("base") base: String,
-        @Query("symbols") symbols: String
-    ): Response<RatesResponse>
-
     //convert from base currency to target currency
     @GET("convert")
     suspend fun convert(
